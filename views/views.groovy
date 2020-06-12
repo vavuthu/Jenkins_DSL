92941d23a9a8de930beaa053de2199e56b0f98fe
vavuthu <vavuthu@redhat.com>
2020-06-12 12:19:48 +0530

    First View job
    Signed-off-by: vavuthu <vavuthu@redhat.com>

---
+++ views/views.groovy
@@ -0,0 +1,20 @@
+
+listView("First Job") {
+    description("This is the First Job")
+    columns {
+        status()
+        buildButton()
+        lastBuildConsole()
+        name()
+        progressBar()
+        lastFailure()
+        lastSuccess()
+        lastDuration()
+        configureProject()
+        disableProject(icon = true)
+    }
+    jobs {
+        regex ( *First*)
+    }
+}
+
